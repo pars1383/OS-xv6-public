aa206ae391b21d6baf0114dedefd487ac7c90196
theniloufar <niloofarmortazavi1382@gmail.com>
2025-02-24 13:24:43 +0000

    -

--- LAB1/init.c
+++ LAB1/init.c
@@ -23,7 +23,7 @@ main(void)
     printf(1, "init: starting sh\n");
     printf(1, "Group #4\n");
     printf(1, "Parsa Alizadeh 810101572\n");
-    printf(1, "Niloofar Mortazavi\n");
+    printf(1, "Niloofar Mortazavi 220701096\n");
     printf(1, "Mohammadreza Khalesi\n");
     pid = fork();
     if(pid < 0){
